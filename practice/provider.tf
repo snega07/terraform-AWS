4e5136f92c4797018fad59b8f01b4d4ce5c50a4a
Snega07 <snegapalanisamy@gmail.com>
2026-06-28 22:35:56 +0530

    state file explained

--- practice/provider.tf
+++ practice/provider.tf
@@ -5,6 +5,15 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket = "project-letz-go-tfstate"
+    key    = "dev/terraform.tfstate"
+    region = "ap-south-1"
+    encrypt = true
+    use_lockfile = true
+  }
+
 }
 provider "aws" {
   region = "ap-south-1"
